76a7200a2d6d4943d33f778bbf035632330306e4
ssstryx <jepsiejoyberon@gmail.com>
2025-12-13 20:22:00 +0800

    refactor: Redesign profile page with Bootstrap card layout

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,25 +1,30 @@
 @extends(Auth::user()->role === 'super_admin' ? 'layouts.super_admin.app' : 'layouts.admin.app')
 
 @section('content')
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
+<div class="container py-5">
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <!-- Profile Information Card -->
+            <div class="card shadow-sm rounded-lg mb-4">
+                <div class="card-body p-4">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
+            <!-- Update Password Card -->
+            <div class="card shadow-sm rounded-lg mb-4">
+                <div class="card-body p-4">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
+            <!-- Delete Account Card -->
+            <div class="card shadow-sm rounded-lg">
+                <div class="card-body p-4">
                     @include('profile.partials.delete-user-form')
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
