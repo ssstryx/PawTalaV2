033b179f769869f2b1dba3553defe7f1554623f7
ssstryx <jepsiejoyberon@gmail.com>
2025-12-13 20:46:01 +0800

    fix: Update logo filename in login page to LogoPawTala.png

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @php use Illuminate\Support\Facades\Vite; @endphp
 <x-guest-layout>
     <div class="text-center mb-4">
-        <img src="{{ Vite::asset('resources/images/logo.png') }}" alt="Logo" class="img-fluid" style="max-height: 150px;">
+        <img src="{{ Vite::asset('resources/images/LogoPawTala.png') }}" alt="Logo" class="img-fluid" style="max-height: 150px;">
     </div>
 
     <!-- Session Status -->
